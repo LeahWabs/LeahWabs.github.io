fb37934216a70174306e6035a88ad321cc5f32a2
LeahWabs <2432523@carrefour.cegepvicto.ca>
2024-12-06 14:49:32 -0500

    version finale

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const cards = document.querySelectorAll('.memory-card');
 let compte=document.getElementById("nombre");
-let number = parseInt(compte.textContent, 10);
+let nombre = parseInt(compte.textContent, 10);
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -14,17 +14,17 @@ function flipCard() {
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
-    // first click
+    /* first click*/
     hasFlippedCard = true;
     firstCard = this;
 
     return;
   }
 
-  // second click
+  /* second click*/
   
   secondCard = this;
-  //la variable comptemove compte le nombre de movement que fait le joueur
+  /*la variable comptemove compte le nombre de movement que fait le joueur*/
  comptemove=comptemove+1;
  console.log(comptemove);
   checkForMatch();
@@ -40,10 +40,10 @@ function disableCards() {
   
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    //la variable move compte le nombre de fois le joueur trouve une paire
+    /*la variable move compte le nombre de fois le joueur trouve une paire*/
     move=move+1;
-// quand move est eguale a 6 donc 12 cartes et que le nombre de chance n'est pas 0 un dialog s'affiche indiquant la victoire 
-    if(move==6&&number>0){
+/*quand move est eguale a 6 donc 12 cartes et que le nombre de chance n'est pas 0 un dialog s'affiche indiquant la victoire */
+    if(move==6&&nombre>0){
       openagain();
       end.innerHTML="Bien joué tu as gagné :)"
     }
@@ -54,12 +54,12 @@ function disableCards() {
 
 function unflipCards() {
   lockBoard = true;
-  //diminue le nombre de chance de un a chaque fois que l'utilisateur retourne deux cartes differente
-  number=number-1;
-  compte.innerHTML=number;
+  /*diminue le nombre de chance de un a chaque fois que l'utilisateur retourne deux cartes differente*/
+  nombre=nombre-1;
+  compte.innerHTML=nombre;
   phrase.innerHTML="Nombre de chances \n manquante:";
-  //quand le nombre de chance est fini un dialog s'affiche indiquant la fin du jeu 
-  if(number==0){
+  /*quand le nombre de chance est fini un dialog s'affiche indiquant la fin du jeu */
+  if(nombre==0){
    openagain();
    end.innerHTML="Tes chances sont finis :( Pas grave tu gagneras une prochaine fois :)"
  }
@@ -104,31 +104,31 @@ if(localStorage.getItem('modalCacher') !== 'false'){
 }else{
   modal.close();
 }
-//cette fonction ferme la boite de dialog quand le bouton fermer est appuyer 
+/*cette fonction ferme la boite de dialog quand le bouton fermer est appuyer */
 function ferme(){
   modal.close();
 }
-//cette fonction ferme la boite de dialog et n'accepte plus la boite
+/*cette fonction ferme la boite de dialog et n'accepte plus la boite*/
 function fermealways(){
   localStorage.setItem('modalCacher', 'false');
   modal.close();
   
   
 }
-// cette fonction permet l'ouverture de la boite de dialog avant l'appuie du bouton ne plus afficher
+/* cette fonction permet l'ouverture de la boite de dialog avant l'appuie du bouton ne plus afficher*/
 function open(){
- // localStorage.removeItem('modalCacher');
+ /*localStorage.removeItem('modalCacher');*/
   dialog.showModal();
 }
-// cette fonction permet l'ouverture de la boite de dialog de la fin
+/*cette fonction permet l'ouverture de la boite de dialog de la fin*/
 function openagain(){
   restart.showModal();
 }
-//affiche le message apres le refus de faire une autre partie
+/*affiche le message apres le refus de faire une autre partie*/
 function findujeu(){
   restart.innerHTML="MERCI D'AVOIR JOUÉ :)"
 }
-// recommence le jeu au debut quand le joueur veut rejouer
+/* recommence le jeu au debut quand le joueur veut rejouer*/
 function playagain(){
   
   location.reload();
